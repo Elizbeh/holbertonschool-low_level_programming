66ba6b3482fa7c3f09a5a426b3c0180978a12f2b
Elizabeh <eliz01_01@yahoo.com>
2022-06-09 11:34:11 +0200

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int r;
+
+	for (r = 0; r < 10; r++)
+		printf("%d", r);
+	printf("\n");
+
+	return (0);
+}
