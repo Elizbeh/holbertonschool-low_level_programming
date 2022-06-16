7f8121bcf150ac6b86edadf5f43487ad3b84656a
Elizabeh <eliz01_01@yahoo.com>
2022-06-16 13:28:56 +0200

    5 more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,21 @@
 /**
   * more_numbers - Write a function that prints 10 times
   * the numbers, from 0 to 14, followed by a new line.
-  * Return: void
+  * Return: Always 0.
  */
 
 void more_numbers(void)
 {
 	int i;
-	int g;
+	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (g = 0; g < 15; g++)
+		for (j = 0; j < 15; j++)
 		{
-			if  (g >= 10)
-				_putchar((g / 10) + '0');
-			_putchar((g % 10) + '0');
+			if  (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
