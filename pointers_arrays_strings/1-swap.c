8a8741929cab46b2847f77d8c99dd5859c62a780
Elizabeh <eliz01_01@yahoo.com>
2022-06-17 10:50:26 +0200

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a: strings
+ * @b: strings
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b);
+{
+	int s;
+
+	 s = a; 
+	 a = b;
+	 b = s;
+}
