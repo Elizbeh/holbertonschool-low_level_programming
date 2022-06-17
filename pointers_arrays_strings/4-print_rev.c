aa9f9a56b9af91b2a044833cde4123964dada588
Elizabeh <eliz01_01@yahoo.com>
2022-06-17 15:24:45 +0200

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ * @s: param
+ * Return: nothing.
+*/
+
+void print_rev(char *s)
+{
+	int m = 0;
+
+	while (s[m] != '\0')
+		m++;
+	while (m > 0)
+	{
+		_putchar(s[m- 1]);
+		m--;
+	}
+	_putchar('\n');
+}
