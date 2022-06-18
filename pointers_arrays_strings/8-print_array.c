900292c3bf7277d343019e2005fc346a816c7d21
Elizabeh <eliz01_01@yahoo.com>
2022-06-18 17:30:07 +0200

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - check the code for
+ * @a: param 1
+ * @n: param 2
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
