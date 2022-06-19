7844eac58091826952c9dfe2fe698a88805f160e
Elizabeh <eliz01_01@yahoo.com>
2022-06-19 18:19:11 +0200

    5-string_toupper.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
-   * _strcmp - fonction
-    * @s1: param
-     * @s2: param
-      * Return: 0 ou n
-       */
+ * _strcmp - fonction
+ * @s1: param
+ * @s2: param
+ * Return: 0 ou n
+ */
+
 int _strcmp(char *s1, char *s2)
 {
-		int i;
+	int i;
 
-			for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-					{
-								if (s1[i] != s2[i])
-												break;
-									}
-				return (s1[i] - s2[i]);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			break;
+	}
+	return (s1[i] - s2[i]);
 }
--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
-   * string_toupper - check the code
-    * @s: param 1
-     * Return: Always 0.
-      */
+ * string_toupper - check the code
+ * @s: param 1
+ * Return: Always 0.
+ */
 
 char *string_toupper(char *s)
 {
-		int i = 0;
+	int i = 0;
 
-			for (i = 0; s[i] != '\0'; i++)
-					{
-								if (s[i] > 96 && s[i] < 123)
-											{
-														s[i] -= 32;
-																}
-									}
-				return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
 }
