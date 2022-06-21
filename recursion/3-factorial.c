89f02619572cf50991bfb2501876c459cabc58fd
Elizabeh <eliz01_01@yahoo.com>
2022-06-21 13:30:05 +0200

    1-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * factorial - main function
+  * @n: parameter
+  * Return: int
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
