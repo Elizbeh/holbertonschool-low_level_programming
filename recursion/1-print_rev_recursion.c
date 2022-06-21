4647fe7f284e93d02c825850a379015733fea6db
Elizabeh <eliz01_01@yahoo.com>
2022-06-21 12:04:56 +0200

    1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _rev_recursion - function
+ * @s: parametre
+ * Return/ nothing
+*/
+
+void _print_rev_recursion(char *s)
+
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
