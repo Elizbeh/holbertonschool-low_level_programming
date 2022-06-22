7644c268dfa9adc24ceaadb1f0c25e5326e7b00c
Elizabeh <eliz01_01@yahoo.com>
2022-06-22 16:05:36 +0200

    0-whatsmyname.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - maint function
+ * @argv: vector string
+ * @argc: count
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
