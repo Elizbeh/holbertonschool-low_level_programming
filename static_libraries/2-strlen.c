15fb34a9b2aee324a9de88b47263918411f65b46
Elizabeh <eliz01_01@yahoo.com>
2022-06-22 11:46:47 +0200

    static libarary

---
+++ static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen  - check the code
+ * @s: parametre
+ * Return: Always 0.
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
