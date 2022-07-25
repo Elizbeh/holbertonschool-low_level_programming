4a4c0e42c386c78bacf998b488006def6213caf7
Elizabeh <eliz01_01@yahoo.com>
2022-07-25 10:14:52 +0200

    5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+/**
+ * get_nodeint_at_index - main function
+ * @head: param1
+ * @index: param2
+ * Return: nothing
+ */
+
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head)
+	{
+		if (count == index)
+			return (head);
+
+		head = head->next;
+		count++;
+	}
+	return (head);
+}
